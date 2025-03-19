8cf87b8ca5ca7052d39bc31477752467c7982a86
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-03-19 15:36:48 -0400

    scores and images

--- 2_rockpaperscissors/rps_game.py
+++ 2_rockpaperscissors/rps_game.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QFont, QPixmap
+
+txt_font = QFont("Times New Roman", 25)
+btn_font = QFont("Arial", 12)
 
 class Window(QWidget):
     def __init__(self):
@@ -10,6 +13,27 @@ class Window(QWidget):
         self.UI()
 
     def UI(self):
+        ####################### Scores #######################
+        self.score_computer_txt = QLabel("Computer Score:", self)
+        self.score_computer_txt.move(30, 20)
+        self.score_computer_txt.setFont(txt_font)
+
+        self.score_player_txt = QLabel("Your Score:", self)
+        self.score_player_txt.move(330, 20)
+        self.score_player_txt.setFont(txt_font)
+
+        ####################### Images #######################
+        self.image_computer = QLabel(self)
+        self.image_computer.setPixmap(QPixmap("images/rock.png"))
+        self.image_computer.move(50, 100)
+
+        self.image_player = QLabel(self)
+        self.image_player.setPixmap(QPixmap("images/rock.png"))
+        self.image_player.move(330, 100)
+
+        self.image_game = QLabel(self)
+        self.image_game.setPixmap(QPixmap("images/game.png"))
+        self.image_game.move(230, 100)
 
         self.show()
 
