c1a00988116c0e1a720e0e2d4e81761d29e9857d
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-03-24 12:12:53 -0400

    change font and color

--- 3_advanded_widgets/openfile_dialog.py
+++ 3_advanded_widgets/openfile_dialog.py
@@ -14,16 +14,31 @@ class Window(QWidget):
         self.editor = QTextEdit()
         file_btn = QPushButton("Open File")
         file_btn.clicked.connect(self.openFile)
+        font_btn = QPushButton("Chenge Font")
+        font_btn.clicked.connect(self.changeFont)
+        color_btn = QPushButton("Change color")
+        color_btn.clicked.connect(self.changeColor)
 
         vbox.addWidget(self.editor)
         vbox.addLayout(hbox)
         hbox.addStretch()
         hbox.addWidget(file_btn)
+        hbox.addWidget(font_btn)
+        hbox.addWidget(color_btn)
         hbox.addStretch()
 
         self.setLayout(vbox)
 
         self.show()
+
+    def changeFont(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.editor.setCurrentFont(font)
+
+    def changeColor(self):
+        color= QColorDialog.getColor()
+        self.editor.setTextColor(color)
 
     def openFile(self):
         url = QFileDialog.getOpenFileName(self, "Open a file", "", "All Files(*);;txt")
