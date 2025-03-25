7ccab1aea931d061ff3f9ba53d50b8df33c3ba21
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-03-25 13:27:28 -0400

    employees app main window and resources

---
+++ 4_employees_app/main.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import *
+import sys
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My Employees")
+        self.setGeometry(450, 150, 750, 600)
+        self.UI()
+        self.show()
+
+    def UI(self):
+        self.mainDesign()
+
+    def mainDesign(self):
+        pass
+
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
