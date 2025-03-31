314f70c8ea06f997f8d8818eb488414565787944
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-03-31 10:18:54 -0500

    creating layouts for add person window

--- 4_employees_app/main.py
+++ 4_employees_app/main.py
@@ -31,20 +31,17 @@ class Main(QWidget):
         self.right_main_layout = QVBoxLayout()
         self.right_top_layout = QHBoxLayout()
         self.right_bottom_layout = QHBoxLayout()
-
         ####################### Adding Layouts #######################
         self.right_main_layout.addLayout(self.right_top_layout)
         self.right_main_layout.addLayout(self.right_bottom_layout)
 
         self.main_layout.addLayout(self.left_layout, 40)  # using aspect ratio for layout
         self.main_layout.addLayout(self.right_main_layout, 60)
-
         ####################### Adding Widgets #######################
         self.right_top_layout.addWidget(self.employee_list)
         self.right_bottom_layout.addWidget(self.btn_new)
         self.right_bottom_layout.addWidget(self.btn_edit)
         self.right_bottom_layout.addWidget(self.btn_del)
-
         ####################### Main Window Layout #######################
         self.setLayout(self.main_layout)
 
@@ -52,6 +49,7 @@ class Main(QWidget):
         self.newEmployee = AddEmployee()
         self.close()
 
+# make a class for each window
 class AddEmployee(QWidget):
     def __init__(self):
         super().__init__()
@@ -61,7 +59,22 @@ class AddEmployee(QWidget):
         self.show()
 
     def UI(self):
+        self.mainDesign()
+        self.layouts()
+
+    def mainDesign(self):
         pass
+
+    def layouts(self):
+        ####################### Main Layouts #######################
+        self.main_layout = QVBoxLayout()
+        self.top_layout = QVBoxLayout()
+        self.bottom_layout = QFormLayout()
+        ####################### Adding Layouts #######################
+        self.main_layout.addLayout(self.top_layout)
+        self.main_layout.addLayout(self.bottom_layout)
+
+        self.setLayout(self.main_layout)
 
 def main():
     app = QApplication(sys.argv)
