62a8fa3bfc78d94da430e8c203da225d575e57d5
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-04-03 22:53:07 -0500

    creating design of update window

--- 4_employees_app/main.py
+++ 4_employees_app/main.py
@@ -148,7 +148,7 @@ class Main(QWidget):
         if self.employee_list.selectedItems():
             person = self.employee_list.currentItem().text()
             id_number, name = person.split(" - ")
-            self.updateWindow = UpdateEmployee()
+            self.updateWindow = EditEmployee(id_number)
             self.close()
         else:
             QMessageBox.information(self, "Info", "Please select a person to update")
@@ -172,7 +172,7 @@ class AddEmployee(QWidget):
         self.main = Main()  # you create another instance of the Main window to go back to
 
     def mainDesign(self):
-        self.setStyleSheet("background-color: white; font-size: 14pt; font-family: Times New Roman;")
+        self.setStyleSheet("background-color: white; font-size: 14pt; font-family: Times New Roman; color: black;")
         ##################### Top Layout Widgets ######################
         self.title = QLabel("Add Person")
         self.title.setStyleSheet("font-size: 24pt; font-family: Arial; font-weight: bold;")  # The style sheet set for the title has preference over the ones set for the window
@@ -264,9 +264,10 @@ class AddEmployee(QWidget):
         else:
             QMessageBox.information(self, "Warning", "Fields cannot be empty")
 
-class UpdateEmployee(QWidget):
-    def __init__(self):
+class EditEmployee(QWidget):
+    def __init__(self, person_id):
         super().__init__()
+        self.person_id = person_id
         self.setWindowTitle("Edit Employee")
         self.setGeometry(450, 150, 350, 600)
         self.UI()
@@ -274,7 +275,73 @@ class UpdateEmployee(QWidget):
         self.show()
 
     def UI(self):
-        pass
+        self.mainDesign()
+        self.layouts()
+        self.getPerson()
+
+    def closeEvent(self, event):
+        self.main = Main()  # you create another instance of the Main window to go back to
+
+    def getPerson(self):
+        print(self.person_id)
+
+    def mainDesign(self):
+        self.setStyleSheet("background-color: white; font-size: 14pt; font-family: Times New Roman; color: black;")
+        ##################### Top Layout Widgets ######################
+        self.title = QLabel("Edit Person")
+        self.title.setStyleSheet(
+            "font-size: 24pt; font-family: Arial; font-weight: bold;")  # The style sheet set for the title has preference over the ones set for the window
+        self.img_add = QLabel()
+        self.img_add.setPixmap(QPixmap('icons/person.png'))
+        ##################### Bottom Layout Widgets ######################
+        self.name_lbl = QLabel("Name: ")
+        self.name_entry = QLineEdit()
+        self.name_entry.setMinimumWidth(210)
+        self.name_entry.setPlaceholderText("Enter Employee Name")
+        self.surname_lbl = QLabel("Surname: ")
+        self.surname_entry = QLineEdit()
+        self.surname_entry.setMinimumWidth(210)
+        self.surname_entry.setPlaceholderText("Enter Employee Surname")
+        self.phone_lbl = QLabel("Phone: ")
+        self.phone_entry = QLineEdit()
+        self.phone_entry.setMinimumWidth(210)
+        self.phone_entry.setPlaceholderText("Enter Employee Phone Number")
+        self.email_lbl = QLabel("Email: ")
+        self.email_entry = QLineEdit()
+        self.email_entry.setMinimumWidth(210)
+        self.email_entry.setPlaceholderText("Enter Employee Email")
+        self.img_lbl = QLabel("Picture: ")
+        self.img_btn = QPushButton("Browse")
+        self.img_btn.setStyleSheet("background-color: orange;")
+        self.addr_lbl = QLabel("Address: ")
+        self.addr_editor = QTextEdit()
+        self.add_btn = QPushButton("Edit")
+        self.add_btn.setStyleSheet("background-color: orange;")
+
+    def layouts(self):
+        ####################### Main Layouts #######################
+        self.main_layout = QVBoxLayout()
+        self.top_layout = QVBoxLayout()
+        self.bottom_layout = QFormLayout()
+        ####################### Adding Layouts #######################
+        self.main_layout.addLayout(self.top_layout)
+        self.main_layout.addLayout(self.bottom_layout)
+        ####################### Adding Widgets to Top Layout #######################
+        self.top_layout.addStretch()
+        self.top_layout.addWidget(self.title)
+        self.top_layout.addWidget(self.img_add)
+        self.top_layout.addStretch()
+        self.top_layout.setContentsMargins(110, 20, 10, 30)  # left, top, right, bottom
+        ####################### Adding Widgets to Bottom Layout #######################
+        self.bottom_layout.addRow(self.name_lbl, self.name_entry)
+        self.bottom_layout.addRow(self.surname_lbl, self.surname_entry)
+        self.bottom_layout.addRow(self.phone_lbl, self.phone_entry)
+        self.bottom_layout.addRow(self.email_lbl, self.email_entry)
+        self.bottom_layout.addRow(self.img_lbl, self.img_btn)
+        self.bottom_layout.addRow(self.addr_lbl, self.addr_editor)
+        self.bottom_layout.addRow("", self.add_btn)
+        ####################### Main Window Layout #######################
+        self.setLayout(self.main_layout)
 
 
 def main():
