863fb082f233c0ab30e4fb040382ea308c2dc5d2
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-04-22 14:50:59 -0400

    adding tool buttons

--- 5_music_player/player.py
+++ 5_music_player/player.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
 
 class Player(QWidget):
     def __init__(self):
@@ -16,12 +18,43 @@ class Player(QWidget):
     def widgets(self):
         self.progress_bar = QProgressBar()
 
+        ########################## Buttons #############################
+        self.add_button = QToolButton()
+        self.add_button.setIcon(QIcon("icons/add.png"))
+        self.add_button.setIconSize(QSize(48, 48))
+        self.add_button.setToolTip("Add song")
+
+        self.shuffle_button = QToolButton()
+        self.shuffle_button.setIcon(QIcon("icons/shuffle.png"))
+        self.shuffle_button.setIconSize(QSize(48, 48))
+        self.shuffle_button.setToolTip("Shuffle the list")
+
+        self.previous_button = QToolButton()
+        self.previous_button.setIcon(QIcon("icons/previous.png"))
+        self.previous_button.setIconSize(QSize(48, 48))
+        self.previous_button.setToolTip("Play previous")
+
+        self.play_button = QToolButton()
+        self.play_button.setIcon(QIcon("icons/play.png"))
+        self.play_button.setIconSize(QSize(48, 48))
+        self.play_button.setToolTip("Play")
+
+        self.next_button = QToolButton()
+        self.next_button.setIcon(QIcon("icons/next.png"))
+        self.next_button.setIconSize(QSize(48, 48))
+        self.next_button.setToolTip("Play next song")
+
+        self.mute_button = QToolButton()
+        self.mute_button.setIcon(QIcon("icons/mute.png"))
+        self.mute_button.setIconSize(QSize(24, 24))
+        self.mute_button.setToolTip("Mute")
+
 
     def layouts(self):
         ########################## Creating Layouts #############################
         self.main_layout = QVBoxLayout()
         self.top_main_layout = QVBoxLayout()
-        self.top_group_box = QGroupBox("Music Player", self)  # Group box is a widget, not a layout
+        self.top_group_box = QGroupBox("Music Player")  # Group box is a widget, not a layout
         self.top_group_box.setStyleSheet("background-color: orange;")
         self.top_layout = QHBoxLayout()
         self.middle_layout = QHBoxLayout()
@@ -30,6 +63,18 @@ class Player(QWidget):
         ######################### Adding Widgets ############################
         ######################### Top Layout Widgets ############################
         self.top_layout.addWidget(self.progress_bar)
+
+        ######################### Middle Layout Widgets ############################
+        self.middle_layout.addStretch()
+        self.middle_layout.addWidget(self.add_button)
+        self.middle_layout.addWidget(self.shuffle_button)
+        self.middle_layout.addWidget(self.previous_button)
+        self.middle_layout.addWidget(self.play_button)
+        self.middle_layout.addWidget(self.next_button)
+        self.middle_layout.addWidget(self.mute_button)
+        self.middle_layout.addStretch()
+
+
         self.top_main_layout.addLayout(self.top_layout)
         self.top_main_layout.addLayout(self.middle_layout)
         self.top_group_box.setLayout(self.top_main_layout)
