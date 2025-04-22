32b1e6e4ef1c0f078cc6b4eaa0049a79137ed5b7
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-04-22 14:35:51 -0400

    creating progress bar

--- 5_music_player/player.py
+++ 5_music_player/player.py
@@ -10,15 +10,32 @@ class Player(QWidget):
         self.show()
 
     def UI(self):
-        pass
+        self.widgets()
+        self.layouts()
 
-    def layout(self):
+    def widgets(self):
+        self.progress_bar = QProgressBar()
+
+
+    def layouts(self):
+        ########################## Creating Layouts #############################
         self.main_layout = QVBoxLayout()
         self.top_main_layout = QVBoxLayout()
-        self.top_group_box = QGroupBox("Music Player", self)
+        self.top_group_box = QGroupBox("Music Player", self)  # Group box is a widget, not a layout
+        self.top_group_box.setStyleSheet("background-color: orange;")
         self.top_layout = QHBoxLayout()
         self.middle_layout = QHBoxLayout()
         self.bottom_layout = QVBoxLayout()
+
+        ######################### Adding Widgets ############################
+        ######################### Top Layout Widgets ############################
+        self.top_layout.addWidget(self.progress_bar)
+        self.top_main_layout.addLayout(self.top_layout)
+        self.top_main_layout.addLayout(self.middle_layout)
+        self.top_group_box.setLayout(self.top_main_layout)
+        self.main_layout.addWidget(self.top_group_box)
+        self.main_layout.addLayout(self.bottom_layout)
+        self.setLayout(self.main_layout)
 
 
 def main():
