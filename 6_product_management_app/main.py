46cedc371fbc218e5a3892e5acf4ab87c25bdd45
silvialpz <74107990+silvialpz@users.noreply.github.com>
2025-04-23 18:21:39 -0400

    starting new project

---
+++ 6_product_management_app/main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Product Management")
+        self.setWindowIcon(QIcon("icons/icon.ico"))
+        self.setGeometry(100, 50, 1350, 750)
+
+        self.UI()
+        self.show()
+
+    def UI(self):
+        pass
+
+def main():
+    App = QApplication(sys.argv)
+    window = Main()
+    sys.exit(App.exec_())
+
+if __name__ == '__main__':
+    main()
